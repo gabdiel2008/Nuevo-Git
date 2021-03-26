9a6229f5160d8274f78a322bd9195850bed49aa8
Eric Garcia <eric.vigueras.grupoinsert@gmail.com>
2021-03-26 08:45:07 -0600

    Se agrego cuerpo al index

--- index.php
+++ index.php
@@ -3,29 +3,32 @@
 <head>
  
 </head>
-    <body>
-        <header>
-            <div class="left_area">
-                <h3> Sistema <span> Autopartes</span></h3>
-            </div>
-
-            <div class="right-area">
-                <a href="" class="logout_btn">Salir</a>               
-            </div>
-        </header>
-
-        <div class="sidebar">
-            <center>
-                <img src="" class="profile_image" alt="">
-                <h4>Eric Garcia</h4>
-            </center>
-            <a href=""><img src="img/menu.png"/><span>Dashboard</span></a>
-            <a href=""><img src="img/contact.png"/><span>Listas</span></a>
-            <a href=""><img src="img/traffic.png"/><span>Productos</span></a>
-            <a href=""><img src="img/total.png"/><span>Ventas</span></a>
-            <a href=""><img class="imagenes" src="img/settings.png"/><span>Ajustes</span></a>
+<body>
+    <header>
+        <div class="left_area">
+            <h3> Sistema <span> Autopartes</span></h3>
         </div>
-            
-    </body>
+
+        <div class="right-area">
+            <a href="" class="logout_btn">Salir</a>               
+        </div>
+    </header>
+
+    <div class="sidebar">
+        <center>
+            <img src="" class="profile_image" alt="">
+            <h4>Eric Garcia</h4>
+        </center>
+        <a href=""><img src="img/menu.png"/><span>Dashboard</span></a>
+        <a href=""><img src="img/contact.png"/><span>Listas</span></a>
+        <a href=""><img src="img/traffic.png"/><span>Productos</span></a>
+        <a href=""><img src="img/total.png"/><span>Ventas</span></a>
+        <a href=""><img class="imagenes" src="img/settings.png"/><span>Ajustes</span></a>
+    </div>
+        
+    <div class="conteent">
+        <h1>Mi primer pagina</h1>
+    </div>
+</body>
   
 </html>
